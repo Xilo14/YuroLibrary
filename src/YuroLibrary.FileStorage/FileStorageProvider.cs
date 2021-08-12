e35c2728f2b1adba69a4e63c847f49aee8ea03c4
Xilosof <pro100temi444@gmail.com>
2021-08-12 14:22:45 +0300

    feat: create class to work with storage

---
+++ src/YuroLibrary.FileStorage/FileStorageProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace YuroLibrary.FileStorage
+{
+    public static class FileStorageProvider
+    {
+        private static readonly string _folderPath = "/TmpFolder";
+        public static Guid SaveFile(Stream stream)
+        {
+            var guid = Guid.NewGuid();
+
+            var fileStream = new FileStream(_folderPath + "/" + guid.ToString(), FileMode.Create);
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.CopyTo(fileStream);
+            fileStream.Dispose();
+
+            return guid;
+        }
+        public static Stream GetFile(Guid FileId)
+        {
+            string[] allFiles = Directory.GetFiles(_folderPath);
+            foreach (string filename in allFiles)
+            {
+                if (Path.GetFileName(filename) == FileId.ToString())
+                {
+                    return new FileStream(_folderPath + "/" + Path.GetFileName(filename), FileMode.Open, FileAccess.Read);
+                }
+            }
+            return null;
+        }
+    }
+}
